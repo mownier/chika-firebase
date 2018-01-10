97d5c55ced365ea1c69d7f27a70ee3f794c3a615
Mounir Ybanez <rinuom91@gmail.com>
2018-01-10 16:22:22 +0800

    Initial commit.

---
+++ ChikaFirebase/ChikaFirebase.h
@@ -0,0 +1,19 @@
+//
+//  ChikaFirebase.h
+//  ChikaFirebase
+//
+//  Created by Mounir Ybanez on 1/10/18.
+//  Copyright © 2018 Nir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ChikaFirebase.
+FOUNDATION_EXPORT double ChikaFirebaseVersionNumber;
+
+//! Project version string for ChikaFirebase.
+FOUNDATION_EXPORT const unsigned char ChikaFirebaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ChikaFirebase/PublicHeader.h>
+
+
