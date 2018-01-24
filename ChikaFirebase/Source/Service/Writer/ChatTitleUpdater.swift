e44355cc9033f2fbaa705163b76db16568ca8f21
Mounir Ybanez <rinuom91@gmail.com>
2018-01-24 15:51:32 +0800

    Added getting of creator.

--- ChikaFirebase/Source/Service/Writer/ChatTitleUpdater.swift
+++ ChikaFirebase/Source/Service/Writer/ChatTitleUpdater.swift
@@ -11,16 +11,42 @@ import FirebaseCommunity
 
 public class ChatTitleUpdater: ChikaCore.ChatTitleUpdater {
 
+    var meID: String
     var database: Database
     
-    public init(database: Database = Database.database()) {
+    public init(meID: String = FirebaseCommunity.Auth.auth().currentUser?.uid ?? "", database: Database = Database.database()) {
+        self.meID = meID
         self.database = database
     }
     
     public func updateTitle(_ title: String, of chatID: ID, completion: @escaping (Result<OK>) -> Void) -> Bool {
-        let rootRef = database.reference()
-        let values = ["chats/\(chatID)/title" : title]
-        rootRef.updateChildValues(values) { error, _ in
+        guard !meID.isEmpty else {
+            completion(.err(Error("current user ID is empty")))
+            return false
+        }
+        
+        let authUserID = meID
+        let updateChildValuesBlock = updateChildValues
+        
+        getCreator(chatID) { result in
+            switch result {
+            case .ok(let creator):
+                var values = ["chat:participant:title/\(chatID)/\(authUserID)" : title]
+                if creator == authUserID {
+                    values["chats/\(chatID)/title"] = title
+                }
+                updateChildValuesBlock(values, completion)
+                
+            case .err(let error):
+                completion(.err(error))
+            }
+        }
+        
+        return true
+    }
+    
+    private func updateChildValues(_ values: [String: Any], _ completion: @escaping (Result<OK>) -> Void) {
+        database.reference().updateChildValues(values) { error, _ in
             guard error == nil else {
                 completion(.err(error!))
                 return
@@ -28,7 +54,22 @@ public class ChatTitleUpdater: ChikaCore.ChatTitleUpdater {
             
             completion(.ok(OK("chat title updated successfully")))
         }
-        return true
+    }
+    
+    private func getCreator(_ chatID: ID, _ completion: @escaping (Result<String>) -> Void) {
+        let chatTitleRef = database.reference().child("chats/\(chatID)/creator")
+        
+        chatTitleRef.observeSingleEvent(of: .value, with: { snapshot in
+            guard let creator = snapshot.value as? String else {
+                completion(.ok(""))
+                return
+            }
+            
+            completion(.ok(creator))
+            
+        }) { error in
+            completion(.err(error))
+        }
     }
     
 }
