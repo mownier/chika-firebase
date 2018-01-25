d0487ed7f7662cf592384cb0b19749bdeaed4ec0
Mounir Ybanez <rinuom91@gmail.com>
2018-01-25 13:47:40 +0800

    Removed getting of participants.

--- ChikaFirebase/Source/Service/Query/ChatQuery.swift
+++ ChikaFirebase/Source/Service/Query/ChatQuery.swift
@@ -13,25 +13,17 @@ public class ChatQuery: ChikaCore.ChatQuery {
 
     var meID: String
     var database: Database
-    var personQuery: ChikaCore.PersonQuery
     var recentChatMessageQuery: ChikaCore.RecentChatMessageQuery
     
-    public init(meID: String, database: Database, personQuery: ChikaCore.PersonQuery, recentChatMessageQuery: ChikaCore.RecentChatMessageQuery) {
+    public init(meID: String, database: Database, recentChatMessageQuery: ChikaCore.RecentChatMessageQuery) {
         self.meID = meID
         self.database = database
-        self.personQuery = personQuery
         self.recentChatMessageQuery = recentChatMessageQuery
     }
     
     public convenience init(meID: String = FirebaseCommunity.Auth.auth().currentUser?.uid ?? "", database: Database = Database.database()) {
-        let personQuery = PersonQuery(database: database)
         let recentChatMessageQuery = RecentChatMessageQuery(database: database)
-        self.init(meID: meID, database: database, personQuery: personQuery, recentChatMessageQuery: recentChatMessageQuery)
-    }
-    
-    public convenience init(meID: String = FirebaseCommunity.Auth.auth().currentUser?.uid ?? "", database: Database = Database.database(), personQuery: ChikaCore.PersonQuery) {
-        let recentChatMessageQuery = RecentChatMessageQuery(database: database)
-        self.init(meID: meID, database: database, personQuery: personQuery, recentChatMessageQuery: recentChatMessageQuery)
+        self.init(meID: meID, database: database, recentChatMessageQuery: recentChatMessageQuery)
     }
     
     public func getChats(for chatIDs: [ID], completion: @escaping (Result<[Chat]>) -> Void) -> Bool {
@@ -68,13 +60,12 @@ public class ChatQuery: ChikaCore.ChatQuery {
     
     private func getChat(_ chatID: ID, _ chatCounterUpdate: @escaping (Chat?) -> Void) {
         let chatsRef = database.reference().child("chats/\(chatID)")
-        let getPersonsBlock = getPersons
+        let getRecentChatMessageBlock = getRecentChatMessage
         
         chatsRef.observeSingleEvent(of: .value, with: { snapshot in
-            guard let info = snapshot.value as? [String : Any],
-                let participants = info["participants"] as? [String: Any] else {
-                    chatCounterUpdate(nil)
-                    return
+            guard let info = snapshot.value as? [String : Any] else {
+                chatCounterUpdate(nil)
+                return
             }
             
             var chat = Chat()
@@ -82,29 +73,22 @@ public class ChatQuery: ChikaCore.ChatQuery {
             chat.title = info["title"] as? String ?? ""
             chat.creatorID = ID(info["creator"] as? String ?? "")
             
-            let personIDs = participants.flatMap({ ID($0.key) })
-            getPersonsBlock(personIDs, chat, chatCounterUpdate)
+            getRecentChatMessageBlock(chat, chatCounterUpdate)
             
         }) { _ in
             chatCounterUpdate(nil)
         }
     }
     
-    private func getPersons(_ personIDs: [ID], _ chat: Chat, _ chatCounterUpdate: @escaping (Chat?) -> Void) {
-        let getRecentChatMessageBlock = getRecentChatMessage
+    private func getRecentChatMessage(_ chat: Chat, _ chatCounterUpdate: @escaping (Chat?) -> Void) {
+        let getChatTitleBlock = getChatTitle
         
-        let _ = personQuery.getPersons(for: personIDs) { result in
+        let _ = recentChatMessageQuery.getRecentChatMessage(of: chat.id) { result in
             switch result {
-            case .ok(let persons):
-                guard personIDs.count == persons.count,
-                    personIDs.sorted(by: { "\($0)" < "\($1)" }) == persons.map({ $0.id }).sorted(by: { "\($0)" < "\($1)" }) else {
-                        chatCounterUpdate(nil)
-                        return
-                }
-                
+            case .ok(let message):
                 var chat = chat
-                chat.participants = persons
-                getRecentChatMessageBlock(chat, chatCounterUpdate)
+                chat.recent = message
+                getChatTitleBlock(chat, chatCounterUpdate)
                 
             case .err:
                 chatCounterUpdate(nil)
@@ -112,25 +96,21 @@ public class ChatQuery: ChikaCore.ChatQuery {
         }
     }
     
-    private func getRecentChatMessage(_ chat: Chat, _ chatCounterUpdate: @escaping (Chat?) -> Void) {
+    private func getChatTitle(_ chat: Chat, _ chatCounterUpdate: @escaping (Chat?) -> Void) {
         let authUserID = meID
+        let chatTitleRef = database.reference().child("chat:participant:title/\(chat.id)/\(authUserID)")
         
-        let _ = recentChatMessageQuery.getRecentChatMessage(of: chat.id) { result in
-            switch result {
-            case .ok(let message):
-                var chat = chat
-                if chat.title.isEmpty {
-                    let filtered = chat.participants.filter({ $0.id != ID(authUserID) })
-                    let mapped = filtered.map({ $0.displayName })
-                    let title = mapped.joined(separator: ", ")
-                    chat.title = title
-                }
-                chat.recent = message
-                chatCounterUpdate(chat)
-                
-            case .err:
-                chatCounterUpdate(nil)
+        chatTitleRef.observeSingleEvent(of: .value, with: { snapshot in
+            var chat = chat
+            
+            if snapshot.exists(), let title = snapshot.value as? String, !title.isEmpty {
+                chat.title = title
             }
+            
+            chatCounterUpdate(chat)
+            
+        }) { _ in
+            chatCounterUpdate(nil)
         }
     }
 
