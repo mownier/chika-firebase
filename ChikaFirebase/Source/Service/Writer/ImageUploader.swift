688d8211ce4b10c209ea70a8367e59532e958046
Mounir Ybanez <rinuom91@gmail.com>
2018-02-22 22:37:59 +0800

    Added project files.

---
+++ ChikaFirebase/Source/Service/Writer/ImageUploader.swift
@@ -0,0 +1,13 @@
+//
+//  ImageUploader.swift
+//  ChikaFirebase
+//
+//  Created by Mounir Ybanez on 2/21/18.
+//  Copyright © 2018 Nir. All rights reserved.
+//
+
+import UIKit
+
+class ImageUploader: AnyObject {
+
+}
